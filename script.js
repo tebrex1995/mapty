8a41493e21abcd1a3f9a6163b79f0563d5532e9d
tebrex1995 <tvrdisica@gmail.com>
2024-11-18 15:10:30 +0100

    added leaflet maps

--- script.js
+++ script.js
@@ -10,3 +10,47 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapEvent) {
+        form.classList.remove('hidden');
+        inputDistance.focus();
+
+        // const { lat, lng } = mapEvent.latlng;
+        // L.marker([lat, lng])
+        //   .addTo(map)
+        //   .bindPopup(
+        //     L.popup({
+        //       maxWidth: 250,
+        //       minWidth: 100,
+        //       autoClose: false,
+        //       closeOnClick: false,
+        //       className: 'running-popup',
+        //     })
+        //   )
+        //   .setPopupContent('Workout')
+        //   .openPopup();
+      });
+    },
+    function () {
+      alert('Could not get your position.');
+    }
+  );
+}
+
+//Form submit
+form.addEventListener('submit', function () {});
